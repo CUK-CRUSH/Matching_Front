cc9c70aab0758a2b505086c3cef0e25bf859fc27
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-04-15 16:57:27 +0900

    Feat : vitest 세팅중

---
+++ vitest.setup.ts
@@ -0,0 +1,6 @@
+import { beforeAll, afterEach, afterAll } from 'vitest';
+import { server } from './src/mocks/server';
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
