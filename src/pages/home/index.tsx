415bd0b511813618260fc959676426953e0c833b
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-04-23 15:19:27 +0900

    Feat - layout 변경

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,52 @@
+import Footer from '@/components/layout/footer';
+import Header from '@/components/layout/header';
+import Layout from '@/components/layout/layout';
+
 export default function Home() {
   return (
-    <div className="flex-1 flex-col w-full flex justify-center">
-      <main className="max-w-[430px] w-full bg-white  relative mx-auto">
-        <div className="relative w-full h-screen overflow-hidden">hu</div>
-      </main>
-    </div>
+    <Layout display="both">
+      <Header />
+      <p>hi1</p>
+      <p>sdfsdf</p>
+      <p>hi2</p>
+      <p>sdfsdf</p>
+      <p>hi3</p>
+      <p>sdfsdf</p>
+      <p>hi4</p>
+      <p>sdfsdf</p>
+      <p>hi5</p>
+      <p>sdfsdf</p>
+      <p>hi6</p>
+      <p>sdfsdf</p>
+      <p>h7</p>
+      <p>sdfsdf</p>
+      <p>hi8</p>
+      <p>sdfsdf</p>
+      <p>hi9</p>
+      <p>sdfsdf</p>
+      <p>hi10</p>
+      <p>sdfsdf</p>
+      <p>hi11</p>
+      <p>sdfsdf</p>
+      <p>hi12</p>
+      <p>sdfsdf</p>
+      <p>hi13</p>
+      <p>sdfsdf</p>
+      <p>hi14</p>
+      <p>sdfsdf</p>
+      <p>hi15</p>
+      <p>sdfsdf</p>
+      <p>hi16</p>
+      <p>sdfsdf</p>
+      <p>hi17</p>
+      <p>sdfsdf</p>
+      <p>hi18</p>
+      <p>sdfsdf</p>
+      <p>hi19</p>
+      <p>sdfsdf</p>
+      <p>hi20</p>
+
+      <Footer />
+    </Layout>
   );
 }
