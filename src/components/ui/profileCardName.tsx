654682a05b27143df023a7650b7d6386dddfbcc6
ys0222 <ggdbstjd0222@gmail.com>
2024-04-25 15:39:19 +0900

    Design : profileCardName 디자인 수정

--- src/components/ui/profileCardName.tsx
+++ src/components/ui/profileCardName.tsx
@@ -5,21 +5,26 @@ const ProfileCardName: React.FC = () => {
  
   const profileCardNameStyle = `relative top-[1.5em] left-[3.5em] `;
   
-  const nameStyle = `font-bold text-[#000]`;
-  const ageStyle = `text-[#2f2f2f] opacity-40	`;
-  const mbtiStyle =`text-[#2f2f2f] opacity-40	`;
-
+  const nameStyle = `font-bold text-[#000] mr-[0.2em]`;
+  const ageStyle = `text-[#2f2f2f] opacity-40 mr-[0.2em]`;
+  const mbtiStyle =`text-[#2f2f2f] opacity-40 mr-[0.2em]`;
+  const barStyle =`text-[#2f2f2f] opacity-40 mr-[0.2em]`
   return (
     <div className={profileCardNameStyle} >
       <span className={nameStyle}>
         최서윤
       </span>
-      
+      <span className={barStyle}>
+        |
+      </span>
       <span className={ageStyle}>
-        | 25세
+        25세
+      </span>
+      <span className={barStyle}>
+        |
       </span>
       <span className={mbtiStyle}>
-      | ENFJ
+        ENFJ
       </span>
     </div>
   );
