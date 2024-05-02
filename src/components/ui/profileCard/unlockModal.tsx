7883525ae03996fab9f83f23d3158d67d708ba4a
ys0222 <ggdbstjd0222@gmail.com>
2024-05-02 16:22:20 +0900

    Feat : 테스트 코드 작성

--- src/components/ui/profileCard/unlockModal.tsx
+++ src/components/ui/profileCard/unlockModal.tsx
@@ -1,5 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 
+import star from '@/assets/profileCard/stars.svg';
+
+import { Button } from '@/components/ui/button';
+
 type UnlockModalProps = {
   setLock: React.Dispatch<React.SetStateAction<boolean>>;
 };
@@ -21,12 +25,27 @@ const UnlockModal: React.FC<UnlockModalProps> = ({ setLock }) => {
     };
   }, [setLock]);
 
+  const modalBackground = `fixed inset-0 bg-[#000] bg-opacity-30 flex justify-center items-center`;
+  const modalStyle = `w-[250px] relative p-8 bg-white rounded-lg flex flex-col justify-start items-center`;
+  const starStyle = `w-[2rem] h-[2rem] mb-[1.5rem]`;
+  const fontStyle = `text-[#2f2f2f] text-[0.8rem]`;
+  const buttonContainer = `flex justify-center mt-4`;
+
   return (
-    <div className="fixed inset-0 bg-[#000] bg-opacity-30 flex justify-center items-center" onClick={() => setLock(prevState => !prevState)}>
-      <div className={`relative p-8 bg-white rounded-lg`} ref={unlockModalRef} onClick={e => e.stopPropagation()} data-testid="modalText">
-        <p>프로필 잠금을 해제할까요?</p>
-        <p>'2' 재화를 소모합니다.</p>
-        
+    <div className={modalBackground} onClick={() => setLock(prevState => !prevState)}>
+      <div className={modalStyle} ref={unlockModalRef} onClick={e => e.stopPropagation()} data-testid="modalText">
+        <img src={star} alt='star' className={starStyle} />
+        <p className={fontStyle}>프로필 잠금을 해제할까요?</p>
+        <p className={fontStyle}>'2' 재화를 소모합니다.</p>
+        <div className={buttonContainer}>
+        <Button className={`mr-4 w-[85px]`} variant="secondary" size="sm" onClick={() => setLock(prevState => !prevState)}>
+          취소
+        </Button>
+        <Button className={`w-[85px] `} variant="default" size="sm">
+          확인
+        </Button>
+        </div>
+
       </div>
     </div>
   );
