a6b81b1bc1f0c5d7bb29d6ffdebb59b5de5ba57e
ys0222 <ggdbstjd0222@gmail.com>
2024-05-10 16:24:59 +0900

    Feat : 이미지 조건부 렌더링 구현

--- src/components/profileCard/post.tsx
+++ src/components/profileCard/post.tsx
@@ -1,17 +1,19 @@
-import message from "@/assets/ProfileCard/message.svg";
-import heart from "@/assets/ProfileCard/heart.svg";
+import offMessage from "@/assets/ProfileCard/offMessage.svg";
+import onMessage from "@/assets/ProfileCard/onMessage.svg";
+import offHeart from "@/assets/ProfileCard/offHeart.svg";
+import onHeart from "@/assets/ProfileCard/onHeart.svg";
 import useProfileCardStore from "@/store/profileCardStore";
 
 const Post = () => {
   
-  const { setOpenMessage } = useProfileCardStore();
+  const { openMessage, setOpenMessage } = useProfileCardStore();
 
   return (
     // margin bottom 으로 스크롤 범위 조작
       <div className={`w-[calc(100%-16%)] mx-[8%] flex justify-end mt-[10px] mb-[150px] ml-[15px]`}>
-        <img src={heart} alt="heart" className={`mr-3`} />
+        <img src={true ? offHeart : onHeart} alt="heart" className={`mr-3`} />
 
-        <img src={message} alt="message" onClick={setOpenMessage}/>
+        <img src={openMessage ? onMessage : offMessage} alt="message" onClick={setOpenMessage}/>
       </div>
     )
 }
