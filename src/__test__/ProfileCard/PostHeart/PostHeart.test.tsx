b82eeff85a8258579e53095022011852b6a1fc50
ys0222 <ggdbstjd0222@gmail.com>
2024-05-16 13:50:45 +0900

    Feat : 좋아요 클릭 시 토스트 테스트 코드 작성

--- src/__test__/ProfileCard/PostHeart/PostHeart.test.tsx
+++ src/__test__/ProfileCard/PostHeart/PostHeart.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen, act } from '@testing-library/react';
 import Post from '@/components/profileCard/post'; // 이미지를 포함하고 있는 컴포넌트
 import userEvent from '@testing-library/user-event';
+import { toast } from '@/components/ui/use-toast';
 
-test('heart image changes correctly', async () => {
+vi.mock('@/components/ui/use-toast', async (importOriginal) => {
+  const actual: () => Promise<unknown> = await importOriginal()
+  return {
+    ...actual,
+    toast: vi.fn(),
+  }
+})
+
+test('좋아요 이미지가 올바르게 바뀌었는지 테스트 합니닷', async () => {
   render(<Post />); // YourComponent가 이미지 변경 로직을 포함
 
   // 초기 상태에서의 이미지 검증 (예: offHeart 상태)
@@ -18,4 +27,8 @@ test('heart image changes correctly', async () => {
 
   // 상태 변경 후의 이미지 검증 (예: onHeart 상태)
   expect(screen.getByAltText('heart')).toHaveAttribute('src', '/src/assets/ProfileCard/onHeart.svg');
+  // toast 함수 호출 확인
+  expect(toast).toHaveBeenCalledWith(expect.objectContaining({
+    title: "좋아요가 전송되었습니다.",
+  }))
 });
