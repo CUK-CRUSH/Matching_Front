bd6de2b917d8e3a9ea3ede2a0e52dcb6c74884ce
ys0222 <ggdbstjd0222@gmail.com>
2024-05-22 13:29:36 +0900

    Design : Tag font Color 변경

--- src/components/matchingList/Tag.tsx
+++ src/components/matchingList/Tag.tsx
@@ -2,13 +2,14 @@ import { TagProps } from "@/type/MatchingList/MatchingList";
 
 const Tag = ({ tag, isDark }: TagProps) => {
   return (
-    <div className={`bg-transparent  px-[4.5px] border-[1px] mb-[4px]
-                border-${isDark ? '[#F9F9F9]' : '[#252525]'} rounded-[20px] mx-[5px] 
-                `} > 
-                <span className={`font-bold text-${isDark ? '[#f9f9f9]' : '[#252525]'} text-xs`}>{tag}</span>
-</div>
+    <div className={`bg-transparent px-[4.5px] border-[1px] mb-[4px]
+                ${isDark ? 'border-[#F9F9F9]' : 'border-[#252525]'} rounded-[20px] mx-[5px] 
+                `} >
+      <span className={`font-bold ${isDark ? 'text-[#f9f9f9]' : 'text-[#252525]'} text-xs`}>{tag}</span>
+    </div>
   )
 }
+
 
 
 export default Tag;
