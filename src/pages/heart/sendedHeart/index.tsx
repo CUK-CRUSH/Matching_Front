09a2f5e989a091655df5445b69e227680cf232de
ys0222 <ggdbstjd0222@gmail.com>
2024-05-22 15:22:26 +0900

    Feat : SendedHeart 컴포넌트 구현

--- src/pages/heart/sendedHeart/index.tsx
+++ src/pages/heart/sendedHeart/index.tsx
@@ -1,9 +1,27 @@
+import Layout from "@/components/layout/layout"
+import Footer from '@/components/layout/footer';
+import MatchingListHeader from "@/components/layout/matchingListHeader";
+import SendedHeartContainer from "@/components/matchingList/SendedHeartContainer";
+import { MOCK_RECEIVE_HEARTS } from "@/fixture/ReceiveHeart";
+import SendedHeartItem from "@/components/matchingList/SendedHeartItem";
+
 const SendedHeart = () => {
-    return(
-        <div>
-            se
-        </div>
-    )
+  return (
+    <Layout backgroundColor='#252525'>
+      <main className="min-h-full h-auto mt-[10vh] bg-matching-list relative flex flex-col pb-[100px]">
+
+        <MatchingListHeader text={'보낸 하트'} background={'#252525'} />
+        <SendedHeartContainer>
+          {MOCK_RECEIVE_HEARTS.map((item, index) => (
+            <SendedHeartItem key={index} {...item} />
+          ))}
+
+        </SendedHeartContainer>
+        <Footer />
+      </main>
+
+    </Layout>
+  )
 }
 
 export default SendedHeart
