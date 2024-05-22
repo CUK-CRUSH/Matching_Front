06efa1788d27030efe8bb0b1ea83af3c5139e1e4
ys0222 <ggdbstjd0222@gmail.com>
2024-05-22 15:39:36 +0900

    Fix : 헤더 스크롤 할때 opacity 때문에 헤더에 글씨 비추는 버그 해결

--- src/components/common/name.tsx
+++ src/components/common/name.tsx
@@ -3,10 +3,10 @@ import { NameProps } from "@/type/Common/Name";
 
 const Name = ({name, age, mbti, isDark} : NameProps) => {
   
-  const fontStyle = `text-[${isDark ? '#fff' : '#2f2f2f'}] text-m opacity-40 mr-[0.2em]`
+  const fontStyle = `${isDark ? 'text-[#858585]' : 'text-[#2f2f2f]'} text-m mr-[0.2em] ${isDark ? '' : 'text-opacity-40'}`;
 
   return (
-    <div className={` `} >
+    <div>
       <span className={`font-bold text-[${isDark ? '#fff' : '#000'}] text-m opacity-100 mr-[0.2em]`}>
         {name}
       </span>
