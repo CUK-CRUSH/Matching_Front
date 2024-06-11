ac596f542bd92f2a3ac4ce09dd7f3b3dc0672239
ys0222 <ggdbstjd0222@gmail.com>
2024-06-11 14:25:31 +0900

    Design : CoupleMusic 디자인 수정

--- src/components/matching/CoupleMusic.tsx
+++ src/components/matching/CoupleMusic.tsx
@@ -8,10 +8,10 @@ const CoupleMusic = ({song,artist} : CoupleProps) => {
       <p data-testid='couple' className={`text-[#2F2F2F] text-s ml-[4%] font-bold my-[8px]`}>
         연인과 함께 듣고싶은 곡
       </p>
-      <div className={`bg-white p-[2%] rounded-[16px] flex flex-col items-center`}>
+      <div className={`bg-white p-[2%] rounded-[16px] flex flex-col items-center relative`}>
         <img src={thumbnail} alt='thumbnail' />
-        <p className={`font-bold text-s text-[#2F2F2F] text-center`}>{song}(따마)</p>
-        <p className={`font-semibold text-m text-[#2F2F2F] text-center`}>{artist}</p>
+        <p className={`absolute bottom-7 font-bold text-s text-[#fff] text-center`}>{song}</p>
+        <p className={`absolute bottom-2 font-semibold text-m text-[#fff] text-center`}>{artist}</p>
       </div>
     </div>
   );
