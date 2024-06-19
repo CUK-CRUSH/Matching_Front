96d85725c55bd96568f3b8404f54b03b46d38fbb
ys0222 <ggdbstjd0222@gmail.com>
2024-06-19 15:46:54 +0900

    Feat : 잠금해제 시 문구 변경

--- src/components/matching/ProfileImage.tsx
+++ src/components/matching/ProfileImage.tsx
@@ -5,7 +5,7 @@ const ProfileImage = ({setOpen,setOpenModal,isLock} : ProfileImageProps) => {
   return (
     <div className={`relative bg-[#2C2C2C] rounded-full w-[3.5rem] h-[3.5rem] p-[1.75rem] `}>
       <img className={`absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2`} 
-           src={true ? lock: 'profileImage'} 
+           src={isLock ? lock: 'profileImage'} 
            alt='lock' 
            onClick={() => isLock ? setOpenModal(prevState => !prevState) : setOpen(true)} />
     </div>
--- src/components/matching/Spread.tsx
+++ src/components/matching/Spread.tsx
@@ -7,7 +7,7 @@ const Spread = ({ setOpenModal,setOpen,isLock }: SpreadProfileCardImageProps) =>
     <div className={`w-full flex justify-center items-center cursor-pointer `}          
     onClick={() => isLock ? setOpenModal(prevState => !prevState) : setOpen(true)} >
     
-      <p className={`font-bold text-[#2F2F2F] text-m flex items-center gap-2`}>잠금해제하고 더 읽기 <img src={doubleDown} alt="spread" /></p>
+      <p className={`font-bold text-[#2F2F2F] text-m flex items-center gap-2`}>{isLock ? '잠금해제하고 더 읽기' : '펼쳐보기'}<img src={doubleDown} alt="spread" /></p>
     </div>
   );
 };
