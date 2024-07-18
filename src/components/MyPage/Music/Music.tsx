6718c63dd95a5ebdda7a56d3af483e69c7fce7da
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-07-18 14:43:38 +0900

    Delete : updatemusic in Store

--- src/components/MyPage/Music/Music.tsx
+++ src/components/MyPage/Music/Music.tsx
@@ -24,7 +24,6 @@ const MusicPage = () => {
     setDeleteLifeMusics,
     deleteLifeMusics,
     setUpdateLifeMusics,
-    updateLifeMusics,
     setCurrentMusic,
   } = useMyPageStore();
   const [cookies] = useCookies(['accessToken']);
