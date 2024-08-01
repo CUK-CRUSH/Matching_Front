21d94e93da2d9208cc9da1f6ba85483b083d73bf
ys0222 <ggdbstjd0222@gmail.com>
2024-08-01 15:21:18 +0900

    Design : Footer 디자인 수정

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,27 +5,26 @@ import offMatching from "@/assets/Layout/offMatching.svg";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const footerStyle = {
-    backgroundImage: `url(${footer})`,
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-  };
-
+  
   return (
-    <footer className="fixed bottom-0 left-0 right-0 max-w-[430px] mx-auto h-[80px] z-50 text-center dark:bg-neutral-600 flex justify-center items-center" 
-          style={footerStyle}>
+    <footer className="fixed bottom-0 left-0 right-0 max-w-[430px] mx-auto
+                       h-[80px] z-50 text-center dark:bg-neutral-600 flex justify-center items-center" 
+          >
+          
+      <img src={footer} alt="Footer" className="absolute inset-0 w-full h-full object-cover" />
+
       {/* 아이콘 배치 */}
-      <div className="flex justify-around w-full max-w-[350px]">
+      <div className="flex justify-around w-full max-w-[350px] relative z-10">
         <Link to='/matchinglist'>
-          <img src={offMessage} alt="Message" />
+          <img className={`z-20`} src={offMessage} alt="Message" />
         </Link>
-        <div className={`w-[5rem] h-[5rem] bg-[#2C2C2C] rounded-full -mt-[62px] flex justify-center items-center ml-1`}>
+        <div className={`w-[5rem] h-[5rem] bg-[#2C2C2C] rounded-full -mt-[62px] flex justify-center items-center ml-1 z-30`}>
           <Link to='/matching'>
-            <img src={offMatching} alt="Matching" />
+            <img src={offMatching} alt="Matching" className="relative z-40" />
           </Link>
         </div>
         <Link to='/mypage'>
-          <img src={offMyPage} alt="My Page" />
+          <img className={`z-20`} src={offMyPage} alt="My Page" />
         </Link>
       </div>
     </footer>
